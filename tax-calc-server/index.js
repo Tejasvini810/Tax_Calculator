b67ee90fee9fdf45beff53d9aed6d038b47f1a18
Tejasvini810 <tejasvinikonkal810@gmail.com>
2023-06-09 14:03:48 +0530

    tax-calculation

--- tax-calc-server/index.js
+++ tax-calc-server/index.js
@@ -28,7 +28,35 @@ app.listen(port, function()  {
   console.log(`Example app listening on port ${port}`)
 })
 
+
 app.post('/postBody',(req,res)=>{
-  console.log(req.body);
-  res.send(req.body);
+
+  const income = req.body.income;
+
+  function calculateIncomeTax(income){
+    let tax=0;
+   
+    if (income <= 30000) {
+     tax = 0;
+   } else if (income > 300000 && income <= 600000) {
+     tax = ((income - 300000) * 5.0) / 100;
+   } else if (income > 600000 && income <=900000) {
+     tax = 15000 + ((income - 600000) * 10.0) / 100; 
+   } else if (income > 900000 && income <=1200000) {
+     tax = 45000 + ((income - 900000) * 15.0) / 100;  
+   } else if (income >  1200000 && income <= 1500000) {
+     tax = 90000 + ((income - 1200000) * 20.0) / 100;
+   } else {
+     tax = 150000 + ((income - 1500000) * 30.0) / 100;
+   }
+      return tax;
+ }
+ 
+ const tax = calculateIncomeTax(income);
+
+
+  console.log(req.body.income);
+  res.send(req.body.income);
+  res.send(req.body.tax);
+
 })
